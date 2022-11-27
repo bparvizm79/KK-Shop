6f4c046a04e48666bd2813749e81bc6fc4e735b0
bparvizm79 <bparvizm79gmail.com>
2022-11-27 15:30:09 +0500

    KK-SHOP

---
+++ app.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector(".nav_menu")
+const menu = document.querySelector(".nav_logo ul")
+
+burger.addEventListener("click", function(){
+    menu.classList.toggle("nav_active")
+})
